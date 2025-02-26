0a766c3782a17a234c81cb2415e377ebf027eb5c
Himanshu Gupta <106430234+NALYRALL@users.noreply.github.com>
2025-02-26 21:54:04 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -18,11 +18,11 @@ int main(void)
     }
     else if (a>=60)
     {
-        printf("D")
+        printf("D");
     }
     else 
     {
-        printf("F")
+        printf("F");
     }
     return 0;
 }
