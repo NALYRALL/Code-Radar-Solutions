5107be731719e12974f30919ed724705b7f322f7
Himanshu Gupta <106430234+NALYRALL@users.noreply.github.com>
2025-02-27 22:15:14 +0530

    Solution to Check if Temperature is Freezing

---
+++ Check if Temperature is Freezing@@6788dd63490637cfc44e1ba3/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%i",&a);
+    if (a<=0)
+    {
+        printf("Freezing");
+    }
+    else
+    {
+        printf("Above Freezing")
+    }
+    return 0;
+}
