5a1b8010771b111755dcfd296fcbe7e1aa594489
Himanshu Gupta <106430234+NALYRALL@users.noreply.github.com>
2025-02-27 09:33:05 +0530

    Solution to Check Voting Eligibility by Age and Citizenship

---
+++ Check Voting Eligibility by Age and Citizenship@@6788dd63490637cfc44e1ba5/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a,b;
+    if (a>=18 && b==1)
+    {
+        printf("Eligible");
+    }
+    else 
+    {
+        printf("Not Eligible");
+    }
+    return 0;
+}
