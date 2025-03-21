5c9899f0df0a7eb65fb4d2158c8724d1c77dff20
Himanshu Gupta <106430234+NALYRALL@users.noreply.github.com>
2025-03-21 10:21:18 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -14,11 +14,30 @@ void sorting(int arr[],int length)
 void smaldiff(int arr[],int length)
 {
     sorting(arr,length);
-    int subarr[length-1],sub2arr[length-1];
+    int subarr[length-1];
     for (int i=0;i<length-1;i++)
     {
         int dif=0;
         dif = arr[i+1]-arr[i];
         subarr[i]=diff;
     }
+    int temp[length-1];
+    for (int i= 0; i< length; i++)
+    {
+        temp[i]= subarr[i];
+    }
+    sorting(temp,length);
+    int var= temp[0];
+    int smallest;
+    for (int i=0;i<length-1;i++)
+    {
+        if (var==subarr[i])
+        {
+            smallest=i
+        }
+    }
+    printf("%d %d",arr[smallest],arr[smallest+1]);
+
+
+
 }
