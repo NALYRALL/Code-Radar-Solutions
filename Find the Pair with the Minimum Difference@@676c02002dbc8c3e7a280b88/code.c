f9f3acfa1d7095a0fd0b590332fac88e514fdfb9
Himanshu Gupta <106430234+NALYRALL@users.noreply.github.com>
2025-03-21 10:36:11 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-void sorting(int arr[],int length)
+int sorting(int arr[], int length)
 {
     for (int i=0;i<length-1;i++)
     {
         for (int j=i+1;j<length;j++)
         {
-            if (arr[i]>arr[j])
+            if (arr[i] > arr[j])
             {
-                int temp=arr[i];
+                int temp= arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
+
             }
         }
     }
